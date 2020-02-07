6537d8b35d5cecb979ff404d9273d83546b66654
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-07 02:50:38 -0500

    partially constructed classes

--- test_request.py
+++ test_request.py
@@ -1,32 +1,68 @@
+""" module docstring """
 import json
 import requests
 
-# Globals
-ENDPOINT_URL = "https://api.github.com/graphql"
-PAT = "PERSONAL_ACCESS_TOKEN"
+# PAT = "PERSONAL_ACCESS_TOKEN"
 
-header = {"Authorization": "token " + PAT}
 
-myquery = """\
-{
-    viewer {
-        name
-    }
-    rateLimit {
-        limit
-        cost
-        remaining
-        resetAt
-    }
-    rep2: repository(name: "nbconvert", owner: "jupyter") {
-        nameWithOwner
-    }
-    rep1: repository(name: "ace", owner: "ajaxorg") {
-        nameWithOwner
-    }
-}
-"""
+class GraphQLQuery:
+    """ Send json graphql queries via HTTP POST requests. This class is probably redundant\
+    as it only simplifies use of query and variables as solely compared to requests """
 
+    def __init__(self, headers, url, query, variables=None):
+        self.headers = headers
+        self.url = url
+        self.query = query
+        # returns empty dict() if variables=None
+        self.variables = variables or dict()
+
+
+    def request():
+        # Returns a requests.Reponse() object
+        r = requests.post(
+            self.url, 
+            json={"query": self.query, "variables": self.variables},
+            headers=self.headers)
+
+
+class GitHubGraphQLQuery(GraphQLQuery):
+    """ has github header, endpoint """
+
+    ENDPOINT_URL = "https://api.github.com/graphql"
+
+    def __init__(self, PAT, query, variables):
+        super().__init__(headers={"Authorization": "token {}".format(PAT)},\
+                         url=ENDPOINT_URL,
+                         query=query, 
+                         variables=variables)
+
+
+class GitStarQuery(GitHubGraphQLQuery):
+    """ Implements graphql query to fetch filtered repos\
+        and fields based on GitStar criterium """
+
+    QUERY = """\
+    {
+        viewer {
+            name
+        }
+        rateLimit {
+            limit
+            cost
+            remaining
+            resetAt
+        }
+        rep2: repository(name: "nbconvert", owner: "jupyter") {
+            nameWithOwner
+        }
+        rep1: repository(name: "ace", owner: "ajaxorg") {
+            nameWithOwner
+        }
+    }
+    """
+
+    def __init__(self, PAT):
+        super().__init__(PAT=PAT, query=QUERY, variables=None)
 
 def print_json(obj):
     """ prints nice json using json.dumps to convert json to str """
@@ -34,7 +70,6 @@ def print_json(obj):
     print(text)
 
 
-# Returns a requests.Reponse() object
-r = requests.post(url=ENDPOINT_URL, json={"query": myquery}, headers=header)
+
 print(r.status_code)
 print_json(r.json())

59d11eb5e0eaf2b97784f4fb73eed56751fd6b9c
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-09 13:56:35 -0500

    fixed QUERY. add response error handling. define global json functions. improve docs

--- gqlquery.py
+++ gqlquery.py
@@ -1,12 +1,23 @@
 """ Implementation of graphQL HTTP POST queries based on 'streaming' generator
     approach. Includes pagination handling and GitHub API specific methods.
 """
+import json
 import requests
 
 
+def json_str(json_dict):
+    """ Use json.dumps to convert json to printable str """
+    return json.dumps(json_dict, indent=4)
+
+
+def print_json(json_dict):
+    """ Prints nice json string """
+    print(json.dumps(json_dict, indent=4))
+
+
 class GraphQLQuery:
-    """ Send json graphql query request and query variables dict via HTTP POST
-    requests
+    """Base class for graphQL queries. Sends json graphql query request and
+        query variables dict via HTTP POST requests.
     """
 
     def __init__(self, headers, url, query, variables=None):
@@ -17,25 +28,26 @@ class GraphQLQuery:
         self.variables = variables or dict()
 
     def gql_response(self):
-        """ Sends HTTP POST query. Returns generator containing a
-            requests.Reponse().json() object """
-        try:
-            return requests.post(
-                url=self.url,
-                headers=self.headers,
-                json={"query": self.query, "variables": self.variables},
-            ).json()
-
-        # ! Need to look into these exceptions more
-
-        except requests.exceptions.HTTPError as http_err:
-            raise http_err
-        except Exception as err:
-            raise err
+        """Sends HTTP POST query. Returns generator containing a
+            requests.Reponse().json() object.
+        """
+        req = requests.post(
+            url=self.url,
+            headers=self.headers,
+            json={"query": self.query, "variables": self.variables},
+        ).json()
+        if "errors" in req:
+            print_json(req)
+            raise requests.RequestException(
+                "Error graphql response from endpoint. Check query"
+            )
+        return req
 
 
 class GitHubGraphQLQuery(GraphQLQuery):
-    """ Incorporates github header and endpoint """
+    """Base class for any GitHub graphQL query. Incorporates github header and
+        endpoint
+    """
 
     ENDPOINT_URL = "https://api.github.com/graphql"
 
@@ -49,14 +61,13 @@ class GitHubGraphQLQuery(GraphQLQuery):
 
 
 class GitStarQuery(GitHubGraphQLQuery):
-    """ Implements graphql query to fetch filtered repos
+    """Implements graphql query to fetch filtered repos
         and fields based on GitStar criterium.
 
         cursors, nextpages is query specific. put here
     """
 
-    QUERY = (
-    """\
+    QUERY = """\
     query searchmp($search_query: String!, $maxitems: Int, $cursor: String) {
        rateLimit {
          limit
@@ -64,7 +75,8 @@ class GitStarQuery(GitHubGraphQLQuery):
          remaining
          resetAt
        }
-       search(query: $search_query, type: REPOSITORY, first: $maxitems, after: $cursor)
+       search(query: $search_query, type: REPOSITORY, """\
+        """first: $maxitems, after: $cursor)
        {
          pageInfo {
            endCursor
@@ -73,7 +85,8 @@ class GitStarQuery(GitHubGraphQLQuery):
          repositoryCount
          edges {
            node {
-
+              # Only __typename is Repository
+              ... on Repository {
                nameWithOwner
                readme: object(expression: "master:README.md") {
                  ... on Blob {
@@ -102,12 +115,11 @@ class GitStarQuery(GitHubGraphQLQuery):
              }
            }
          }
+        }
       }
      """
-    )
 
-    TEST_QUERY = (
-    """\
+    TEST_QUERY = """\
     query searchmp($search_query: String!, $maxitems: Int, $cursor: String) {
         rateLimit {
           limit
@@ -133,7 +145,6 @@ class GitStarQuery(GitHubGraphQLQuery):
         }
     }
     """
-    )
 
     VARIABLES = {
         "search_query": "archived:false mirror:false stars:>0 "
@@ -144,21 +155,23 @@ class GitStarQuery(GitHubGraphQLQuery):
 
     def __init__(self, PAT, maxitems=1):
         super().__init__(
-            PAT=PAT, query=GitStarQuery.TEST_QUERY, variables=GitStarQuery.VARIABLES
+            PAT=PAT,
+            query=GitStarQuery.TEST_QUERY,
+            variables=GitStarQuery.VARIABLES,
         )
         self.variables["maxitems"] = maxitems
 
     def generator(self):
-        """ Pagination generator iterated upon query response boolean 'hasNextPage'.
+        """Pagination generator iterated upon query response boolean 'hasNextPage'.
             Calls gql_response() then updates cursor and hasNextPage.
             Exceptions (e.g. StopIteration) are to be handled outside of class.
         """
-        hasNextPage = True
-        while hasNextPage:
+        nextpage = True
+        while nextpage:
             gen = self.gql_response()
-            print(gen)
             # Update cursor
-            self.variables["cursor"] = gen["data"]["search"]["pageInfo"]["endCursor"]
+            self.variables["cursor"] = \
+                gen["data"]["search"]["pageInfo"]["endCursor"]
             # Update hasNextPage
-            hasNextPage = gen["data"]["search"]["pageInfo"]["hasNextPage"]
+            nextpage = gen["data"]["search"]["pageInfo"]["hasNextPage"]
             yield gen
--- test_request.py
+++ test_request.py
@@ -3,21 +3,22 @@ import json
 import gqlquery
 
 # PAT = "PERSONAL_ACCESS_TOKEN"
+PAT = "423f853ce3ba7ed0bb0588e3fa8588f970a58923"
 
 
-def jstr(json_obj):
-    """ Use json.dumps to convert json to nice str """
-    return json.dumps(json_obj, indent=4)
+def json_str(json_dict):
+    """ Use json.dumps to convert json to printable str """
+    return json.dumps(json_dict, indent=4)
 
 
-def jprint(json_obj):
+def print_json(json_dict):
     """ Prints nice json string """
-    print(json.dumps(json_obj, indent=4))
+    print(json.dumps(json_dict, indent=4))
 
 
 def main():
     """ Test the class implementations """
-    a2 = gqlquery.GitStarQuery(PAT, maxitems=10)
+    a2 = gqlquery.GitStarQuery(PAT, maxitems=1)
     b = a2.generator()
     jprint(next(b))
     jprint(next(b))
@@ -29,3 +30,6 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         exit()
+nextpage
+printjson
+print_json

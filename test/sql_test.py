f04fb6df80a315bf812b7b556ba79b1f0d8be308
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-11 15:17:51 -0500

    sketch pyodbc usage

--- test/sql_test.py
+++ test/sql_test.py
@@ -23,9 +23,39 @@ cnxn = pyodbc.connect(
 )
 cursor = cnxn.cursor()
 cursor.execute(
-    "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid"
+    """
+    CREATE TABLE [IF NOT EXISTS] mytable (
+    a_col INTEGER DEFAULT 0,
+    b_col
+"""
+)
+
+
+cursor.execute(
+    """
+    SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+        FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p
+        ON pc.productcategoryid = p.productcategoryid
+"""
 )
 row = cursor.fetchone()
 while row:
     print(str(row[0]) + " " + str(row[1]))
     row = cursor.fetchone()
+
+# pass parameters with question marks
+cursor.execute(
+    """
+    select user_id, user_name
+      from users
+     where last_logon < ?
+       and bill_overdue = ?
+""",
+    [datetime.date(2001, 1, 1), "y"],
+)
+
+# Use commit on INSERT and CREATE
+cursor.execute(
+    "insert into products(id, name) values (?, ?)", "pyodbc", "awesome library"
+)
+cnxn.commit()

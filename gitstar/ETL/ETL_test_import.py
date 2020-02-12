964a3cbda514496ae58c26b67c88e112513befef
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-12 15:34:00 -0500

    further import test for config

--- gitstar/ETL/ETL_test_import.py
+++ gitstar/ETL/ETL_test_import.py
@@ -1,12 +1,12 @@
 # from gitstar import config  # no work
 # import config  # no work
-#from .. import config  # no work
-#from gitstar import gqlquery  # no work
+# from .. import config  # no work
+# from gitstar import gqlquery  # no work
+from gitstar.config_test import config
 
-from gqlquery import GraphQLQuery #Sibling module works fine
+from gqlquery import GraphQLQuery  # Sibling module works fine
 
 
-# print(config.PAT)
+print(config.PAT)
 
 print(dir(GraphQLQuery))
-

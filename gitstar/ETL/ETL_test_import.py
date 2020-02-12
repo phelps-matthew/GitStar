4015f5e8c8541cd750b096c98d0e013f154eb2ed
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-12 15:04:38 -0500

    more tests upon ETL_test_import

--- gitstar/ETL/ETL_test_import.py
+++ gitstar/ETL/ETL_test_import.py
@@ -1,4 +1,12 @@
-# from gitstar import config
-# import config
-from .. import config
-print(config.PAT)
+# from gitstar import config  # no work
+# import config  # no work
+#from .. import config  # no work
+#from gitstar import gqlquery  # no work
+
+from gqlquery import GraphQLQuery #Sibling module works fine
+
+
+# print(config.PAT)
+
+print(dir(GraphQLQuery))
+

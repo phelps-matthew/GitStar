b85b5a8a20129c7e5dd5360bfaaa62a83cd59e90
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-12 15:58:55 -0500

    edit tel test import

--- gitstar/ETL/ETL_test_import.py
+++ gitstar/ETL/ETL_test_import.py
@@ -2,7 +2,7 @@
 # import config  # no work
 # from .. import config  # no work
 # from gitstar import gqlquery  # no work
-from gitstar.configtest import configx
+from gitstar.configtest import configx  # Sibling package does not work
 
 from gqlquery import GraphQLQuery  # Sibling module works fine
 

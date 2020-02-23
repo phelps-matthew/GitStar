db1da92aae44e0f45653e07153fd70fb0325ffca
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-23 00:39:18 -0500

    improve feature graphing, add pngs

--- gitstar/models/feature_normalization_discover.py
+++ gitstar/models/feature_normalization_discover.py
@@ -9,41 +9,57 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_PATH = BASE_DIR / "dataset"
 IMG_PATH = BASE_DIR / "features"
 IMG_PATH.mkdir(parents=True, exist_ok=True)
-FILENAME_test = "query1.csv"
 FILENAME = "gs_table_v2.csv"
+
+
+def gen_hist(img_path, data, qtl=None):
+    """Generate histograms of columns"""
+    img_path.mkdir(parents=True, exist_ok=True)
+    for col in data:
+        if qtl:
+            qrange = (data[col].min(), 1.1*data[col].quantile(qtl))
+        else:
+            qrange = (data[col].min(), data[col].max())
+        ax = data[col].plot.hist(bins=100, title=col, range=qrange)
+        fig = ax.get_figure()
+        fig.savefig(
+            img_path / ("{}_hist.png".format(col)),
+            transparent=False,
+            dpi=300,
+            bbox_inches="tight",  # fit bounds of figure to plot
+        )
+        plt.close(fig)
+
+
+def gen_scatter(img_path, data, qtl=None):
+    """Generate scatter plots of columns vs stargazers"""
+    img_path.mkdir(parents=True, exist_ok=True)
+    for col in data.iloc[:, 1:]:
+        ax = data.plot.scatter(x=col, y="stargazers", s=5, alpha=0.5)
+        if qtl:
+            ax.set_xlim(right=1.1*data[col].quantile(qtl))
+        ax.set_ylim(top=100000)  # outlier 996.ICU
+        fig = ax.get_figure()
+        fig.savefig(
+            img_path / ("{}_star.png".format(col)),
+            transparent=False,
+            dpi=300,
+            bbox_inches="tight",  # fit bounds of figure to plot
+        )
+        plt.close(fig)
 
 
 def main():
     data = pd.read_csv(DATA_PATH / FILENAME)
     ti = int(arrow.get("2017-06-01").format("X"))
     tf = int(arrow.get("2018-01-01").format("X"))
-    data_sub = data[data["created"].between(ti, tf)]
-    # print histograms of columns
-    for col in data_sub.iloc[:, 1:]:
-        qtl = data_sub[col].quantile(0.9)
-        ax = data_sub[col].plot.hist(bins=100, title=col, range=(0, qtl))
-        fig = ax.get_figure()
-        # plt.show()
-        fig.savefig(
-            IMG_PATH / ("{}_hist.png".format(col)),
-            transparent=False,
-            dpi=300,
-            bbox_inches="tight",  # fit bounds of figure to plot
-        )
-        plt.close(fig)
-    # print scatter plot of col vs stargazers
-    for col in data_sub.iloc[:, 1:]:
-        ax = data_sub.plot.scatter(x=col, y="stargazers", s=5, alpha=0.5)
-        qtl = data_sub[col].quantile(0.9)
-        ax.set_xlim([-0.1 * qtl, 1.1 * qtl])
-        fig = ax.get_figure()
-        fig.savefig(
-            IMG_PATH / ("{}_star.png".format(col)),
-            transparent=False,
-            dpi=300,
-            bbox_inches="tight",  # fit bounds of figure to plot
-        )
-        plt.close(fig)
+    star_min = 100
+    # data_sub = data[data["created"].between(ti, tf)]
+    #data = data[data["stargazers"] >= star_min]
+    mypath = "full_qtl_80"
+    qtl = 0.8
+    gen_hist(IMG_PATH/mypath, data, qtl=qtl)
+    gen_scatter(IMG_PATH/mypath, data, qtl=qtl)
 
 
 if __name__ == "__main__":

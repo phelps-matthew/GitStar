f30305443e1e6f89ca0ca6e1ce91165dbc47f28c
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-27 13:30:23 -0500

    add datanorm.py

---
+++ gitstar/models/datanorm.py
@@ -0,0 +1,12 @@
+
+from pathlib import Path
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import minmax_scale
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import PowerTransformer

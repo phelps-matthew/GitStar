7c83567ba42416f536e8466ba60d78a7d4bc8c9b
Matthew Phelps <50338490+mgp333@users.noreply.github.com>
2020-02-29 17:42:55 -0500

    include package dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
 from setuptools import setup, find_packages
 
-setup(name='gitstarproject', version='1.0', packages=find_packages())
+setup(
+    name="gitstarproject",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "pathlib",
+        "requests",
+        "logging",
+        "matplotlib",
+        "arrow",
+        "pyodbc",
+        "json",
+        "time",
+    ],
+)
